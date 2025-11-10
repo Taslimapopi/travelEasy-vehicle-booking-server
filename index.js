bdef2845926c9fd05e1a6e78b197bca36b07d6c9
taslima <garryanderson337@gmail.com>
2025-11-10 17:37:17 +0600

    myvehicle roue added

--- index.js
+++ index.js
@@ -104,6 +104,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/bookings/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollections.deleteOne(query);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
